8a453ef3230302b06c4eb42ee66552d5d7aa6c76
pr5 <pr5@gnome.su>
2023-06-19 19:32:44 +0300

    подчищал то что нагадил в комментах

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 2 # по углам колбасит
 #потом починю. может быть. 
+# а может это из за пп и комично маленького количества пикселей на экране
 
 
 #func _ready():
@@ -12,7 +13,7 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("Rightward") - Input.get_action_strength("Leftward")
 	input_vector.y = -(Input.get_action_strength("Forward") - Input.get_action_strength("Backward"))
-	input_vector = input_vector.normalized() #<-- а если нормализовать вектор то будет колбасить также и при скорости больше 50
+	input_vector = input_vector.normalized()  
 	if input_vector != Vector2.ZERO:
 		velocity = input_vector
 	else:
