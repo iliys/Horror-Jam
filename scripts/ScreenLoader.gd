5f5ee1ae6411a3d1cb9f10f772671d9d3b288b89
pr5 <pr5@gnome.su>
2023-06-20 11:28:05 +0300

    Переделал механизм переключения комнат. Теперь нужно использовать только гуй и сохранять позицию относительно комнаты

--- scripts/ScreenLoader.gd
+++ scripts/ScreenLoader.gd
@@ -1,13 +1,19 @@
-
 extends Area2D
+
 @export var GOTO_X: int 
 @export var GOTO_Y: int 
-
+@export_enum("Horizontal", "Vertical", "Exact") var TRANSITION_TYPE: String = "Exact"
 @onready var camera = $"../../Camera2D"
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		body.position.x=GOTO_X
-		body.position.y=GOTO_Y
+		match TRANSITION_TYPE:
+			"Horizontal":
+				body.position.x=GOTO_X
+			"Vertical":
+				body.position.y=GOTO_Y
+			"Exact":
+				body.position.x=GOTO_X
+				body.position.y=GOTO_Y
 		print(body.name)
 
